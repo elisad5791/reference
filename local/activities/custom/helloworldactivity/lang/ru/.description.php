c673f4a8ecc8b0629354afc31b4af0e0c9c14cdf
elisad5791 <elisad5791@mail.ru>
2024-07-04 02:21:57 +0500

    commit

--- local/activities/custom/helloworldactivity/lang/ru/.description.php
+++
@@ -1,4 +0,0 @@
-<?php
-$MESS['HELLOWORLD_DESCR_NAME']  = 'Привет мир!';
-$MESS['HELLOWORLD_DESCR_DESCR'] = 'Генерирует привественное сообщение';
-$MESS['HELLOWORLD_DESCR_FIELD_TEXT'] = 'Текст сообщения';
